ddccec8c7d439dc256185ff2c4493d8a3eb9bf48
Erik Stone <spologas@gmail.com>
2021-10-16 22:47:13 -0700

    changed some routes, blogs still not loading after signup

--- public/displayLogIn.js
+++ public/displayLogIn.js
@@ -22,17 +22,17 @@ const selectLoginElement = async (e) => {
 const selectSignupElement = async (e) => {
     e.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
+    // const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    if(name && email && password) {
+    if(email && email && password) {
         const response = await fetch('/api/users', {
             method: "POST",
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ email, password }),
             headers: { 'Content-type': 'application/json'},
         });
         if (response.ok) {
-            document.location.replace('/posted');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
